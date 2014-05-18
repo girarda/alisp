b331a50a8b1a5ed043145e5cac678409e177bf8e
Alexandre Girard <girarda.92@gmail.com>
2014-05-17 18:04:02 -0700

    Add command history.

--- main.c
+++ main.c
@@ -3,10 +3,51 @@
 #include <editline/history.h>
 
 #include "atom.h"
+#include "error.h"
+#include "parser.h"
 
 int main(int argc, char** argv) {
-    Atom atom = cons(make_sym("foo"), make_int(2));
-    print_expr(atom);
+    char *input;
+
+//    input = "(test)";
+//    const char *p = input;
+
+//    Error err;
+//    Atom expr;
+
+//    err = read_expr(p, &p, &expr);
+
+//    switch (err) {
+//    case ERROR_OK:
+//        print_expr((expr));
+//        putchar('\n');
+//        break;
+//    case ERROR_SYNTAX:
+//        puts("Syntax error");
+//        break;
+//    }
+
+    while ((input = readline("> ")) != NULL) {
+        add_history(input);
+        const char *p = input;
+        Error err;
+        Atom expr;
+
+        err = read_expr(p, &p, &expr);
+
+        switch (err) {
+        case ERROR_OK:
+            print_expr((expr));
+            putchar('\n');
+            break;
+        case ERROR_SYNTAX:
+            puts("Syntax error");
+            break;
+        }
+
+        free(input);
+    }
+
     return 0;
 }
 
